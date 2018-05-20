3a5120950d35b14f397de6fdf0b0bf10f23373e7
DLG <daniel-lozano>
2018-05-19 19:01:23 -0500

    basic coding

---
+++ diagonalicing_blas.h
@@ -0,0 +1,13 @@
+//
+//  diagonalicing_blas.h
+//  
+//
+//  Created by Daniel Lozano on 19/05/18.
+//
+
+#ifndef diagonalicing_blas_h
+#define diagonalicing_blas_h
+
+#include <stdio.h>
+
+#endif /* diagonalicing_blas_h */
---
+++ matrix_multiplication.h
@@ -0,0 +1,13 @@
+//
+//  matrix_multiplication.h
+//  
+//
+//  Created by Daniel Lozano on 19/05/18.
+//
+
+#ifndef matrix_multiplication_h
+#define matrix_multiplication_h
+
+#include <stdio.h>
+
+#endif /* matrix_multiplication_h */
